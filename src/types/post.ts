ba754f153f65115e0337ee36599e3e4ba5b9d093
Harry Keightley <harrykeightley@outlook.com>
2022-12-02 16:48:52 +1000

    Better typing for pages and posts

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,5 +1,15 @@
-export type Post = {
-  [key: string]: string;
-};
+export interface Page {
+  title: string;
+  slug: string;
+  content: string;
+  tags?: string[];
+}
+
+export interface Post extends Page {
+  order: number;
+  author: string;
+  date: string;
+  draft?: boolean;
+}
 
 export default Post;
